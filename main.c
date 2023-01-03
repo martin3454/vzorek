da8714e6eaea07a64d002951ba5020587801c0d0
martin3454 <81327855+martin3454@users.noreply.github.com>
2023-01-03 17:00:31 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define blen 100
+#define blen 3
 #define maxradek 100
-#define vzlen 4
-#define pomlen 100
+#define vzlen 3
+#define pomlen 3
 
 struct fronta{
 	char buffer[blen];
 	char pom_buf[pomlen];
 	int b_zac, b_kon, pom_zac, pom_kon;
+	int b_pocet, pom_pocet;
 	
 };
 
@@ -44,12 +45,12 @@ int main(int argc, char **argv)
 	FILE *file = fopen("auto.txt", "r");
 	
 	//char radek[maxradek];
-	char *vzorek = "auto";
+	char *vzorek = "aha";
 	
 	struct fronta Fronta;
 	
 	init_fronta(&Fronta);
-
+	
 	int z, j = 0; 
 	if((z = nactiznak(&Fronta, file)) != EOF) put_pombuf(&Fronta, z);
 	
@@ -75,6 +76,7 @@ int main(int argc, char **argv)
 				
 				
 		z = nactiznak(&Fronta, file);
+		printf("%c", z);
 		put_pombuf(&Fronta, z);
 	}
 	
@@ -114,11 +116,11 @@ char nactiznak(struct fronta *pt, FILE *fp){
 }
 
 int jeFrontaprazdna(struct fronta *pt){
-	return (pt->b_zac + 1 == pt->b_kon);
+	return (pt->b_pocet == 0);
 }
 
 int jePombufprazdny(struct fronta *pt){
-	return (pt->pom_zac + 1 == pt->pom_kon);
+	return (pt->pom_pocet == 0);
 }
 
 void pombufTofronta(struct fronta *pt){
@@ -131,11 +133,13 @@ void pombufTofronta(struct fronta *pt){
 }
 
 void init_fronta(struct fronta* pt){
-	pt->b_kon = 0;
-	pt->b_zac = -1;
+	pt->b_kon = -1;
+	pt->b_zac = 0;
+	pt->b_pocet = 0;
 	
-	pt->pom_zac = -1;
-	pt->pom_kon = 0;
+	pt->pom_zac = 0;
+	pt->pom_kon = -1;
+	pt->pom_pocet = 0;
 	
 	
 	memset(pt->buffer, 0, blen);
@@ -143,62 +147,66 @@ void init_fronta(struct fronta* pt){
 }
 
 void clear_mainfronta(struct fronta *pt){
-	pt->b_kon = 0;
-	pt->b_zac = -1;
+	pt->b_kon = -1;
+	pt->b_zac = 0;
 	memset(pt->buffer, 0, blen);
 }
 
 
 void clear_pombuf(struct fronta *pt){
-	pt->pom_kon = 0;
-	pt->pom_zac = -1;
+	pt->pom_kon = -1;
+	pt->pom_zac = 0;
 	memset(pt->pom_buf, 0, pomlen);
 }
 
 
 void put_mainfronta(struct fronta *pt, char item){
 	
-	if((pt->b_kon) % blen == pt->b_zac || pt->b_kon >= blen){
+	if(pt->b_pocet == blen){
 		puts("fronta je plno");
 		return;
 	}else{
-		//printf("vkladam do buf: %c\n", item);
-		pt->b_kon = pt->b_kon % blen;
+		printf("vkladam do buf: %c\n", item);
+		pt->b_kon = (pt->b_kon + 1) % blen;
         pt->buffer[pt->b_kon] = item;
-        pt->b_kon++;
-	} 
-}
-
-void put_pombuf(struct fronta *pt, char item){
-	
-	if((pt->pom_kon) % pomlen == pt->pom_zac || pt->pom_kon >= pomlen){
-		puts("pom je plno");
-		return;
-	}else{
-		//puts("vkladam do pom");
-		//printf("vkladam do pom: %c\n", item);
-		pt->pom_kon = pt->pom_kon % pomlen;
-        pt->pom_buf[pt->pom_kon] = item;
-        pt->pom_kon++;
+        pt->b_pocet++;
 	} 
 }
 
 void get_mainfronta(struct fronta *pt, char *item){
 	
-	if(pt->b_zac + 1 == pt->b_kon){
+	if(pt->b_pocet == 0){
 		printf("fronta je prazdna");
 		return;
 	}
-	*item = pt->buffer[++pt->b_zac];
+	pt->b_zac = pt->b_zac % blen;
+	*item = pt->buffer[pt->b_zac++];
+	printf("ziskavam znak : %c\n", *item);
+	--pt->b_pocet;
+}
+
+void put_pombuf(struct fronta *pt, char item){
+	
+	if(pt->pom_pocet == pomlen){
+		puts("pom je plno");
+		return;
+	}else{
+		printf("vkladam do pom: %c\n", item);
+		pt->pom_kon = (pt->pom_kon + 1) % pomlen;
+        pt->pom_buf[pt->pom_kon] = item;
+        pt->pom_pocet++;
+	} 
 }
 
 void get_pombuf(struct fronta *pt, char *item){
 	
-	if(pt->pom_zac + 1 == pt->pom_kon){
-		printf("pom je prazdna");
+	if(pt->pom_pocet == 0){
+		printf("fronta je prazdna");
 		return;
 	}
-	*item = pt->pom_buf[++pt->pom_zac];
+	pt->pom_zac = pt->pom_zac % pomlen;
+	*item = pt->pom_buf[pt->pom_zac++];
+	--pt->pom_pocet;
 }
 
 
